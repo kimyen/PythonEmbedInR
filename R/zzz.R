691b99ee47dc23ab216ba7eacfabc5d5e04c4163
Kimyen <kimyen0816@gmail.com>
2017-12-21 10:51:44 -0800

    there is a '/' in $R_ARCH

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,19 @@
 
 pathToPythonLibraries<-function(libname, pkgname) {
-	# Note: 'pythonLibs' is defined in configure.win
-	pathToPythonLibraries<-file.path(libname, pkgname, "pythonLibs", Sys.getenv("R_ARCH"))
-	pathToPythonLibraries<-gsub("/", "\\", pathToPythonLibraries, fixed=T)
-	pathToPythonLibraries
+  # Note: 'pythonLibs' is defined in configure.win
+  # removing the '/'
+  arch <- substring(Sys.getenv("R_ARCH"), 2)
+  pathToPythonLibraries<-file.path(libname, pkgname, "pythonLibs", arch)
+  pathToPythonLibraries<-gsub("/", "\\", pathToPythonLibraries, fixed=T)
+  pathToPythonLibraries
 }
 
 # on Windows we need to add Python dll's to library search path
 addPythonLibrariesToWindowsPath<-function(libname, pkgname) {
-	if (Sys.info()['sysname']=="Windows") {
-		extendedPath <- sprintf("%s%s%s", Sys.getenv("PATH"), .Platform$path.sep, pathToPythonLibraries(libname, pkgname))
-		Sys.setenv(PATH=extendedPath)
-	}
+  if (Sys.info()['sysname']=="Windows") {
+    extendedPath <- sprintf("%s%s%s", Sys.getenv("PATH"), .Platform$path.sep, pathToPythonLibraries(libname, pkgname))
+    Sys.setenv(PATH=extendedPath)
+  }
 }
 
 # NOTE:  This is one of several places the version is hard coded.  See also AutodetectPython.R, configure, configure.win 
@@ -21,18 +23,19 @@ PYTHON_VERSION<-"3.5"
   # at the compile time a flag is set which can
   # be accessed by using the function isDllVersion 
   addPythonLibrariesToWindowsPath(libname, pkgname)
-	packageRootDir<-file.path(libname, pkgname)
-	Sys.setenv(PYTHONHOME=packageRootDir)
-	
-	#set pythonhome and pythonpath so python knows where to look for python modules
-	if (Sys.info()['sysname']=="Windows"){
-		pythonPathEnv<-paste(file.path(packageRootDir, "pythonLibs", Sys.getenv("R_ARCH")), file.path(packageRootDir, "pythonLibs", Sys.getenv("R_ARCH"), "Lib\\site-packages"), sep=";")
-	}else{
-		pythonPathEnv<-file.path(packageRootDir, "lib")
-	}
-	
+  packageRootDir<-file.path(libname, pkgname)
+  Sys.setenv(PYTHONHOME=packageRootDir)
+  
+  #set pythonhome and pythonpath so python knows where to look for python modules
+  if (Sys.info()['sysname']=="Windows"){
+    arch <- substring(Sys.getenv("R_ARCH"), 2)
+    pythonPathEnv<-paste(file.path(packageRootDir, "pythonLibs", arch), file.path(packageRootDir, "pythonLibs", arch, "Lib\\site-packages"), sep=";")
+  }else{
+    pythonPathEnv<-file.path(packageRootDir, "lib")
+  }
+  
   Sys.setenv(PYTHONPATH=pythonPathEnv)
-	
+  
   # Unloading it and then reloading it is a hacky way of making less modifications to the original code:
   # In the NAMESPACE file, we load load PythonInR.so with "useDynLib(PythonInR)"
   # However, the symbols are not loaded globally(RTLD_GLOBAL) and will cause issues
@@ -43,25 +46,25 @@ PYTHON_VERSION<-"3.5"
   # Reference: http://r.789695.n4.nabble.com/question-re-error-message-package-error-quot-functionName-quot-not-resolved-from-current-namespace-td4663892.html
   library.dynam.unload("PythonEmbedInR", packageRootDir)
   library.dynam("PythonEmbedInR", pkgname, libname, local=FALSE)
-	
-	# On Mac load the ssl libraries
-	if (Sys.info()['sysname']=='Darwin') {
-		sharedObjectFile<-system.file("lib/libcrypto.1.0.0.dylib", package="PythonEmbedInR")
-		dyn.load(sharedObjectFile, local=FALSE)
-		sharedObjectFile<-system.file("lib/libssl.1.0.0.dylib", package="PythonEmbedInR")
-		dyn.load(sharedObjectFile, local=FALSE)
-		Sys.setenv(SSL_CERT_FILE=system.file(paste0("lib/python", PYTHON_VERSION, "/site-packages/pip/_vendor/requests/cacert.pem"), package="PythonEmbedInR"))
-	}
-	
-	pyConnect()
+  
+  # On Mac load the ssl libraries
+  if (Sys.info()['sysname']=='Darwin') {
+    sharedObjectFile<-system.file("lib/libcrypto.1.0.0.dylib", package="PythonEmbedInR")
+    dyn.load(sharedObjectFile, local=FALSE)
+    sharedObjectFile<-system.file("lib/libssl.1.0.0.dylib", package="PythonEmbedInR")
+    dyn.load(sharedObjectFile, local=FALSE)
+    Sys.setenv(SSL_CERT_FILE=system.file(paste0("lib/python", PYTHON_VERSION, "/site-packages/pip/_vendor/requests/cacert.pem"), package="PythonEmbedInR"))
+  }
+  
+  pyConnect()
   
   if (Sys.info()['sysname']=="Linux") {
-		# if we build a static library, libpythonX.Xm.a, instead of a dynamically linked one,
-		# libpythonX.Xm.so.1.0, then don't do the following
-		sharedObjectFile<-system.file(paste0("lib/libpython", PYTHON_VERSION, "m.so.1.0", package="PythonEmbedInR"))
-		if (file.exists(sharedObjectFile)) {
-			dyn.load(sharedObjectFile, local=FALSE)
-		}
+    # if we build a static library, libpythonX.Xm.a, instead of a dynamically linked one,
+    # libpythonX.Xm.so.1.0, then don't do the following
+    sharedObjectFile<-system.file(paste0("lib/libpython", PYTHON_VERSION, "m.so.1.0", package="PythonEmbedInR"))
+    if (file.exists(sharedObjectFile)) {
+      dyn.load(sharedObjectFile, local=FALSE)
+    }
   }
   invisible(NULL)
 }
