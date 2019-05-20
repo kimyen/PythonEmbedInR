1224a7250112355077e60407bf6fd43af757f7f8
KimYen Ladia <kimyen0816@gmail.com>
2019-05-20 12:12:21 -0700

    do not use new feature

--- jenkins.sh
+++ jenkins.sh
@@ -113,7 +113,11 @@ elif  [[ $label = $WINDOWS_LABEL_PREFIX* ]]; then
   ## build the binary for Windows
   for f in ${PACKAGE_NAME}_${PACKAGE_VERSION}.tar.gz
   do
-     R CMD INSTALL --build "$f" --library=../RLIB --no-test-load --merge-multiarch
+    if [[ $RVERS == 3.6]]; then
+      R CMD INSTALL --build "$f" --library=../RLIB --no-test-load --merge-multiarch --no-staged-install
+    else
+      R CMD INSTALL --build "$f" --library=../RLIB --no-test-load --merge-multiarch
+    fi
   done
   ## This is very important, otherwise the source packages from the windows build overwrite 
   ## the ones created on the unix machine.
